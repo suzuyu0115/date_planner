030ebe6d9bb37de7ea586f0fec6743d0259b5ec0
suzuyu0115 <szkyut0115@gmail.com>
2024-07-04 01:41:20 +0900

    Add: geminiの定義

---
+++ src/app/lib/gemini.ts
@@ -0,0 +1,14 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY
+
+if (!apiKey) {
+  throw new Error('Gemini API Key が見つかりません');
+}
+
+const modelName = 'gemini-1.5-flash-latest';
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({ model: modelName })
+
+export default model;
