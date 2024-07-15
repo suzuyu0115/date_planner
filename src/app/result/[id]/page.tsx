bf403ce27abbddff9712befb9944c31549d8127e
suzuyu0115 <szkyut0115@gmail.com>
2024-07-16 00:44:57 +0900

    Add: 無効なリンクにアクセスした場合、404に飛ばす処理の実装

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -1,7 +1,15 @@
+import { getPlan } from '@/utils/planStorage';
 import PlanResultContent from '../PlanResultContent';
+import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default function ResultPage({ params }: { params: { id: string } }) {
+  const planData = getPlan(params.id);
+
+  if (!planData) {
+    notFound();
+  }
+
   return <PlanResultContent id={params.id} />;
 }
